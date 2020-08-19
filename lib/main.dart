5bbacbfc5c744c9eccf6a70f1175bdfa30570676
Crazy <3346148964@qq.com>
2020-08-19 15:28:21 +0800

    导航栏和导航栏下的标签栏,DefaultTabController

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,83 @@
 import 'package:flutter/material.dart';
+import './dome/listview_demo.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+      theme: ThemeData(primarySwatch: Colors.yellow),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+            backgroundColor: Colors.grey[100],
+
+            // 导航栏
+            appBar: AppBar(
+              // 标题
+              title: Text('FLUTTER'),
+
+              // 左边按钮
+              leading: IconButton(
+                icon: Icon(Icons.menu),
+                tooltip: 'Navigration',
+                onPressed: () => debugPrint('Navigration button is pressed'),
+              ),
+
+              // 右边按钮
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.search),
+                  tooltip: 'search',
+                  onPressed: () => debugPrint('search button is pressed'),
+                )
+              ],
+              elevation: 0.0,
+
+              // 做个标签栏
+              bottom: TabBar(
+                unselectedLabelColor: Colors.black38,
+                indicatorColor: Colors.black54,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorWeight: 1.0,
+                tabs: <Widget>[
+                  Tab(icon: Icon(Icons.local_florist)),
+                  Tab(icon: Icon(Icons.local_activity)),
+                  Tab(icon: Icon(Icons.local_bar)),
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: <Widget>[
+                Tab(
+                    icon: Icon(
+                  Icons.local_florist,
+                  size: 120,
+                  color: Colors.black12,
+                )),
+                Tab(
+                    icon: Icon(
+                  Icons.local_activity,
+                  size: 120,
+                  color: Colors.black12,
+                )),
+                Tab(
+                    icon: Icon(
+                  Icons.local_bar,
+                  size: 120,
+                  color: Colors.black12,
+                )),
+              ],
+            )));
+  }
+}
---
+++ lib/model/post.dart
@@ -0,0 +1,100 @@
+class Post {
+  const Post({this.title, this.author, this.imageUrl});
+
+  final String title;
+  final String author;
+  final String imageUrl;
+}
+
+final List<Post> posts = [
+  Post(
+    title: 'Candy Shop',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1597810887&di=f7e9390e863018d567cda5fac197fe91&src=http://img.08087.cc/uploads/20191101/18/1572604557-ymPfegZvKa.jpg',
+  ),
+  Post(
+    title: 'Childhood in a picture',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2575448845,3521203520&fm=26&gp=0.jpg',
+  ),
+  Post(
+    title: 'Contained',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1410722167,209803709&fm=26&gp=0.jpg',
+  ),
+  Post(
+    title: 'Dragon',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=4204099679,67881237&fm=26&gp=0.jpgg',
+  ),
+  Post(
+    title: 'Free Hugs',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2474575555,1750376000&fm=26&gp=0.jpg',
+  ),
+  Post(
+    title: 'Gravity Falls',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597821043522&di=7d5be19091336c741845a71bfae579f8&imgtype=0&src=http%3A%2F%2Fhbimg.huabanimg.com%2F3332f75bb9fb4e9a37aab37a636b378a386a567d188de-OT6lJl_fw236',
+  ),
+  Post(
+    title: 'Icecream Truck',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2244041329,2128136394&fm=26&gp=0.jpg',
+  ),
+  Post(
+    title: 'keyclack',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3385248079,2789851865&fm=26&gp=0.jpg',
+  ),
+  Post(
+    title: 'Overkill',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1035491838,2699574696&fm=26&gp=0.jpg',
+  ),
+  Post(
+    title: 'Say Hello to Barry',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=74727122,2466177663&fm=26&gp=0.jpg',
+  ),
+  Post(
+    title: 'Space Skull',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1721958687,135990257&fm=26&gp=0.jpg',
+  ),
+  Post(
+    title: 'The Old Fashioned',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1386428252,3710927982&fm=26&gp=0.jpg',
+  ),
+  Post(
+    title: 'Tornado',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3727457671,105392468&fm=26&gp=0.jpg',
+  ),
+  Post(
+    title: 'Undo',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2318771334,691365250&fm=26&gp=0.jpg',
+  ),
+  Post(
+    title: 'White Dragon',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=375087312,3130218692&fm=26&gp=0.jpg',
+  )
+];
